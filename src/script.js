51b7e8f65ac21ff27ac78af2f2dc7c8a7e18e996
Cassie <capnrengade@gmail.com>
2026-03-24 21:20:47 -0400

    Major updates

--- src/script.js
+++ src/script.js
@@ -1,5 +1,19 @@
 const toggle = document.getElementById("darkModeToggle");
 
+// Set initial icon
+if (document.body.classList.contains("dark-mode")) {
+  toggle.textContent = "☀️";
+} else {
+  toggle.textContent = "🌙";
+}
+
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
+
+  // Switch icon
+  if (document.body.classList.contains("dark-mode")) {
+    toggle.textContent = "☀️"; // light mode icon
+  } else {
+    toggle.textContent = "🌙"; // dark mode icon
+  }
 });
